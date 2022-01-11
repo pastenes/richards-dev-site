9fd440f703fe4d402049264d8a5cbec627a87630
Richard Pastenes <richard@pastenes.com>
2022-01-10 20:47:27 -0800

    Adds Google fonts with Fontsource

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,4 @@
 import './src/styles/global.css'
+
+import '@fontsource/poppins/700.css'
+import '@fontsource/pt-serif'
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ const Header = () => {
   return (
     <section>
       <h1 className="text-3xl font-bold underline">Richard Pastenes</h1>
+      <p>This is body text</p>
     </section>
   )
 }
