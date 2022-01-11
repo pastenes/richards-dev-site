9ace0acd6d470a470e235867422796bb60081676
Richard Pastenes <richard@pastenes.com>
2022-01-10 21:12:22 -0800

    Adds Google fonts to Tailwind

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,3 @@
 import './src/styles/global.css'
-
 import '@fontsource/poppins/700.css'
 import '@fontsource/pt-serif'
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <section>
-      <h1 className="text-3xl font-bold underline">Richard Pastenes</h1>
+      <h1 className="text-xl">Richard Pastenes</h1>
       <p>This is body text</p>
     </section>
   )
