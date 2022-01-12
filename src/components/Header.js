49fe165db929654dd1e7d7b847194e4e72a701d6
Richard Pastenes <richard@pastenes.com>
2022-01-12 15:00:07 -0800

    Adds icon links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect, useRef } from 'react'
 import BIRDS from 'vanta/dist/vanta.birds.min'
 import { StaticImage } from "gatsby-plugin-image"
+import { FaLinkedin } from "@react-icons/all-files/fa/FaLinkedin";
+import { FaGithub } from "@react-icons/all-files/fa/FaGithub";
+import { FaTwitter } from "@react-icons/all-files/fa/FaTwitter";
 
 const Header = () => {
   const [vantaEffect, setVantaEffect] = useState(0)
@@ -35,7 +38,21 @@ const Header = () => {
         <div className="flex-auto justify-items-center self-center w-1/2 p-14 ">
           <div className='max-w-md'>
             <h1 className="text-5xl">Richard Pastenes</h1>
-            <p>I've been obsessed with figuring out how things work from a young age. From tinkering with my toys to getting in trouble for taking apart (and putting back together) pretty much every electrinic in my childhood homo, I was driven to pursue the analytical, yet creative, field of Electronic Engineering. On top of having an Associates degree in Web design and Bachelors in Electronic Engineering, re-write this whole thing.</p>
+            <p>I've been obsessed with figuring out how things work from a young age. From tinkering with my toys to getting in trouble for taking apart (and putting back together) pretty much every electronic in my childhood home, I was driven to pursue the analytical, yet creative, field of Software Developer. On top of having an Associates degree in Web design and Bachelors in Electronic Engineering, re-write this whole thing.</p>
+
+            <p>I'm not just a coder who was born in the driest desert in the world-- I'm also an avid photographer, hiker, and adventure seeker. (I mean, who would move to a new country to marry the love of their life other than someone who loves adventure?) When I'm not busy transforming code into beautiful websites, you can find him hiking, fixing bikes, or taking photos.</p>
+
+            <div className="flex justify-start my-8">
+              <a className="text-white text-4xl pr-2" href='https://www.linkedin.com/in/richardpastenes/' target='_blank' rel="noreferrer">
+                <FaLinkedin className='hover:drop-shadow-lg'/>
+              </a>
+              <a className="text-white text-4xl pr-2" href='https://github.com/pastenes' target='_blank' rel="noreferrer">
+                <FaGithub className='hover:drop-shadow-lg'/>
+              </a>
+              <a className="text-white text-4xl pr-2" href='https://github.com/pastenes' target='_blank' rel="noreferrer">
+                <FaTwitter className='hover:drop-shadow-lg'/>
+              </a>
+            </div>
           </div>
         </div>
         <div className='flex-auto self-end w-1/2'>
