a3b309fdf5bf0660c4abed24b9cd20f3238a8b86
Richard Pastenes <richard@pastenes.com>
2022-01-12 16:41:53 -0800

    Made it responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,14 +33,13 @@ const Header = () => {
     }
   }, [vantaEffect])
   return (
-      <section ref={myRef} className="bg-neutral h-screen w-screen flex">
-        {/* TODO: add container with padding to entire site */}
-        <div className="flex-auto justify-items-center self-center w-1/2 p-14 ">
+      <section ref={myRef} className="bg-neutral h-screen w-screen flex flex-col lg:flex-row">
+        <div className="flex-auto justify-items-center self-center w-3/4 lg:w-1/2 pt-14 px-0 pb-14 lg:p-14 ">
           <div className='max-w-md'>
             <h1 className="text-5xl">Richard Pastenes</h1>
-            <p>I've been obsessed with figuring out how things work from a young age. From tinkering with my toys to getting in trouble for taking apart (and putting back together) pretty much every electronic in my childhood home, I was driven to pursue the analytical, yet creative, field of Software Developer. On top of having an Associates degree in Web design and Bachelors in Electronic Engineering, re-write this whole thing.</p>
+            <p>I've been obsessed with figuring out how things work from a young age. From tinkering with my toys to getting in trouble for taking apart (and putting back together) pretty much every electronic in my childhood home, I was driven to pursue the analytical, yet creative, field of Software Developer.</p>
 
-            <p>I'm not just a coder who was born in the driest desert in the world-- I'm also an avid photographer, hiker, and adventure seeker. (I mean, who would move to a new country to marry the love of their life other than someone who loves adventure?) When I'm not busy transforming code into beautiful websites, you can find him hiking, fixing bikes, or taking photos.</p>
+            {/* <p>I'm not just a coder who was born in the driest desert in the world-- I'm also an avid photographer, hiker, and adventure seeker. (I mean, who would move to a new country to marry the love of their life other than someone who loves adventure?) When I'm not busy transforming code into beautiful websites, you can find him hiking, fixing bikes, or taking photos.</p> */}
 
             <div className="flex justify-start my-8">
               <a className="text-white text-4xl pr-2" href='https://www.linkedin.com/in/richardpastenes/' target='_blank' rel="noreferrer">
@@ -55,12 +54,12 @@ const Header = () => {
             </div>
           </div>
         </div>
-        <div className='flex-auto self-end w-1/2'>
+        <div className='max-w-xs self-end w-1/2 lg:justify-self-end'>
           <StaticImage 
             src="../images/richard-pastenes.png" 
             alt="Richard Pastnes being awesome"
             placeholder='tracedSVG'
-            layout='fixed'
+            layout='constrained'
             width={380}
           />
         </div>
