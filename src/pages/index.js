3a14e09160704f37693def76a3be0c016e889cbe
Richard Pastenes <richard@pastenes.com>
2022-01-13 21:19:05 -0800

    Adds seciton scroll

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FullPage, Slide } from 'react-full-page';
 import Layout from '../components/Layout'
 // import Seo from '../components/Seo'
 // import { Helmet } from 'react-helmet'
@@ -9,7 +10,11 @@ const Page = () => {
     <Layout>
       {/* <Seo description={pageData.seo.metaDesc} title={pageData.seo.title} canonical={pageData.seo.canonical}/> */}
       {/* <Helmet bodyAttributes={{ class: `page ${bodyTag}` }} /> */}
-      <Header />
+      <FullPage controls={false} scrollMode='normal'>
+        <Slide>
+          <Header />
+        </Slide>
+      </FullPage>
     </Layout>
   )
 }
