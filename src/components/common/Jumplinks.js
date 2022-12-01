f69a45b437afac18a128ac7107bb845caa3edb28
Richard Pastenes <richard@pastenes.com>
2022-12-01 17:04:03 -0500

    Centers nav chevron

--- src/components/common/Jumplinks.js
+++ src/components/common/Jumplinks.js
@@ -4,10 +4,10 @@ import AnchorLink from 'react-anchor-link-smooth-scroll'
 import { Link } from "gatsby";
 
 const Jumplinks = () => (
-  <nav className="text-green text-center sticky -top-20 -mt-20 ml-14 lg:ml-nav z-20 bg-dark inline-flex flex-col p-3 font-fira-code">
+  <nav className="text-green text-center sticky -top-20 -mt-20 ml-14  lg:ml-nav z-20 bg-dark inline-flex flex-col p-3 font-fira-code">
     <AnchorLink className='py-4' href='#about'>
     <div className="bg-dark text-5xl text-center">
-      <FaChevronDown className="animate-bounce"/>
+        <FaChevronDown className="animate-bounce ml-1" />
     </div>
     </AnchorLink>
     <AnchorLink className='py-4' href='#about'>About</AnchorLink>
